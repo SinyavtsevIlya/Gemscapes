a96dd24de553d35a74c40bb8c7b6ab6bb16fb1fc
Ilya Sinyavtsev <sinyavtsevilya@gmail.com>
2022-02-19 19:40:45 +0300

    Project structure rework

---
+++ Assets/_Client/Features/Battle/Presentation/Systems/UI/Screens/BattleScreenSystem.cs
@@ -0,0 +1,44 @@
+﻿using Nanory.Lex;
+using Nanory.Lex.Lifecycle;
+using Client.Rpg;
+
+namespace Client.Battle
+{
+    [UpdateInGroup(typeof(ScreenSystemGroup))]
+    public sealed class BattleScreenSystem : EcsSystemBase
+    {
+        protected override void OnUpdate()
+        {
+            foreach (var ownerEntity in Filter()
+            .With<ScreensStorage>()
+            .With<CreatedEvent>()
+            .End())
+            {
+                var screen = this.GetScreen<BattleScreen>(ownerEntity);
+                screen.CloseButton.onClick.AddListener(() => 
+                {
+                    var later = GetCommandBufferFrom<BeginSimulationECBSystem>();
+                    later.Add<FinishBattleRequest>(ownerEntity);
+                });
+            }
+
+            foreach (var ownerEntity in Filter()
+            .With<OpenEvent<BattleScreen>>()
+            .End())
+            {
+                var screen = Get<OpenEvent<BattleScreen>>(ownerEntity).Value;
+
+                this.BindWidget(ownerEntity, screen.HealthWidget);
+            }
+
+            foreach (var ownerEntity in Filter()
+            .With<CloseEvent<BattleScreen>>()
+            .End())
+            {
+                var screen = Get<CloseEvent<BattleScreen>>(ownerEntity).Value;
+
+                this.UnbindWidget(ownerEntity, screen.HealthWidget);
+            }
+        }
+    }
+}
